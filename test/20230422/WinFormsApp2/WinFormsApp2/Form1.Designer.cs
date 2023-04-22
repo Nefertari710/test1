82ca15eb3e9c772ee5e3fca1f506e43f27ba9dab
Nefertari710 <2583741979@qq.com>
2023-04-22 21:39:54 +1200

    d

---
+++ test/20230422/WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WinFormsApp2
+{
+    partial class Cache
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelShowStatus = new System.Windows.Forms.Label();
+            this.labelShowStatus1 = new System.Windows.Forms.Label();
+            this.labelShowStatus2 = new System.Windows.Forms.Label();
+            this.buttonDeleteAllFile = new System.Windows.Forms.Button();
+            this.labelDeleteFIleStatus = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelShowStatus
+            // 
+            this.labelShowStatus.AutoSize = true;
+            this.labelShowStatus.Location = new System.Drawing.Point(12, 9);
+            this.labelShowStatus.Name = "labelShowStatus";
+            this.labelShowStatus.Size = new System.Drawing.Size(93, 15);
+            this.labelShowStatus.TabIndex = 0;
+            this.labelShowStatus.Text = "labelShowStatus";
+            // 
+            // labelShowStatus1
+            // 
+            this.labelShowStatus1.AutoSize = true;
+            this.labelShowStatus1.Location = new System.Drawing.Point(12, 24);
+            this.labelShowStatus1.Name = "labelShowStatus1";
+            this.labelShowStatus1.Size = new System.Drawing.Size(99, 15);
+            this.labelShowStatus1.TabIndex = 1;
+            this.labelShowStatus1.Text = "labelShowStatus1";
+            // 
+            // labelShowStatus2
+            // 
+            this.labelShowStatus2.AutoSize = true;
+            this.labelShowStatus2.Location = new System.Drawing.Point(12, 39);
+            this.labelShowStatus2.Name = "labelShowStatus2";
+            this.labelShowStatus2.Size = new System.Drawing.Size(99, 15);
+            this.labelShowStatus2.TabIndex = 2;
+            this.labelShowStatus2.Text = "labelShowStatus2";
+            // 
+            // buttonDeleteAllFile
+            // 
+            this.buttonDeleteAllFile.Location = new System.Drawing.Point(38, 112);
+            this.buttonDeleteAllFile.Name = "buttonDeleteAllFile";
+            this.buttonDeleteAllFile.Size = new System.Drawing.Size(135, 23);
+            this.buttonDeleteAllFile.TabIndex = 4;
+            this.buttonDeleteAllFile.Text = "buttonDeleteAllFile";
+            this.buttonDeleteAllFile.UseVisualStyleBackColor = true;
+            this.buttonDeleteAllFile.Click += new System.EventHandler(this.buttonDeleteAllFile_Click);
+            // 
+            // labelDeleteFIleStatus
+            // 
+            this.labelDeleteFIleStatus.AutoSize = true;
+            this.labelDeleteFIleStatus.Location = new System.Drawing.Point(179, 120);
+            this.labelDeleteFIleStatus.Name = "labelDeleteFIleStatus";
+            this.labelDeleteFIleStatus.Size = new System.Drawing.Size(115, 15);
+            this.labelDeleteFIleStatus.TabIndex = 5;
+            this.labelDeleteFIleStatus.Text = "labelDeleteFIleStatus";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(38, 141);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(256, 276);
+            this.textBoxLog.TabIndex = 6;
+            this.textBoxLog.Text = "textBoxLog";
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Location = new System.Drawing.Point(334, 54);
+            this.textBoxData.Multiline = true;
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBoxData.Size = new System.Drawing.Size(284, 363);
+            this.textBoxData.TabIndex = 7;
+            this.textBoxData.Text = "textBoxData";
+            // 
+            // Cache
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxData);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.labelDeleteFIleStatus);
+            this.Controls.Add(this.buttonDeleteAllFile);
+            this.Controls.Add(this.labelShowStatus2);
+            this.Controls.Add(this.labelShowStatus1);
+            this.Controls.Add(this.labelShowStatus);
+            this.Name = "Cache";
+            this.Text = "Cache";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelShowStatus;
+        private Label labelShowStatus1;
+        private Label labelShowStatus2;
+        private Button buttonDeleteAllFile;
+        private Label labelDeleteFIleStatus;
+        private TextBox textBoxLog;
+        private TextBox textBoxData;
+    }
+}
---
+++ test/20230422/WinFormsApp3/WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WinFormsApp3
+{
+    partial class Server
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelShowStatus = new Label();
+            labelShowStatus1 = new Label();
+            labelShowStatus2 = new Label();
+            labelShowStatus3 = new Label();
+            listBoxAllData = new ListBox();
+            label1 = new Label();
+            listBoxServerData = new ListBox();
+            label2 = new Label();
+            buttonAuth = new Button();
+            labelShowStatus4 = new Label();
+            buttonInsertFile = new Button();
+            SuspendLayout();
+            // 
+            // labelShowStatus
+            // 
+            labelShowStatus.AutoSize = true;
+            labelShowStatus.Location = new Point(12, 9);
+            labelShowStatus.Name = "labelShowStatus";
+            labelShowStatus.Size = new Size(93, 15);
+            labelShowStatus.TabIndex = 0;
+            labelShowStatus.Text = "labelShowStatus";
+            // 
+            // labelShowStatus1
+            // 
+            labelShowStatus1.AutoSize = true;
+            labelShowStatus1.Location = new Point(12, 24);
+            labelShowStatus1.Name = "labelShowStatus1";
+            labelShowStatus1.Size = new Size(99, 15);
+            labelShowStatus1.TabIndex = 1;
+            labelShowStatus1.Text = "labelShowStatus1";
+            // 
+            // labelShowStatus2
+            // 
+            labelShowStatus2.AutoSize = true;
+            labelShowStatus2.Location = new Point(12, 39);
+            labelShowStatus2.Name = "labelShowStatus2";
+            labelShowStatus2.Size = new Size(99, 15);
+            labelShowStatus2.TabIndex = 2;
+            labelShowStatus2.Text = "labelShowStatus2";
+            // 
+            // labelShowStatus3
+            // 
+            labelShowStatus3.AutoSize = true;
+            labelShowStatus3.Location = new Point(12, 54);
+            labelShowStatus3.Name = "labelShowStatus3";
+            labelShowStatus3.Size = new Size(99, 15);
+            labelShowStatus3.TabIndex = 3;
+            labelShowStatus3.Text = "labelShowStatus3";
+            // 
+            // listBoxAllData
+            // 
+            listBoxAllData.FormattingEnabled = true;
+            listBoxAllData.ItemHeight = 15;
+            listBoxAllData.Location = new Point(48, 140);
+            listBoxAllData.Name = "listBoxAllData";
+            listBoxAllData.Size = new Size(174, 229);
+            listBoxAllData.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 122);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 5;
+            label1.Text = "All File List";
+            // 
+            // listBoxServerData
+            // 
+            listBoxServerData.FormattingEnabled = true;
+            listBoxServerData.ItemHeight = 15;
+            listBoxServerData.Location = new Point(287, 140);
+            listBoxServerData.Name = "listBoxServerData";
+            listBoxServerData.Size = new Size(167, 229);
+            listBoxServerData.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(287, 118);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Available File List";
+            // 
+            // buttonAuth
+            // 
+            buttonAuth.Location = new Point(147, 378);
+            buttonAuth.Name = "buttonAuth";
+            buttonAuth.Size = new Size(75, 23);
+            buttonAuth.TabIndex = 8;
+            buttonAuth.Text = "Authorized";
+            buttonAuth.UseVisualStyleBackColor = true;
+            buttonAuth.Click += buttonAuth_Click;
+            // 
+            // labelShowStatus4
+            // 
+            labelShowStatus4.AutoSize = true;
+            labelShowStatus4.Location = new Point(12, 68);
+            labelShowStatus4.Name = "labelShowStatus4";
+            labelShowStatus4.Size = new Size(99, 15);
+            labelShowStatus4.TabIndex = 9;
+            labelShowStatus4.Text = "labelShowStatus4";
+            // 
+            // buttonInsertFile
+            // 
+            buttonInsertFile.Location = new Point(117, 114);
+            buttonInsertFile.Name = "buttonInsertFile";
+            buttonInsertFile.Size = new Size(105, 23);
+            buttonInsertFile.TabIndex = 10;
+            buttonInsertFile.Text = "buttonInsertFile";
+            buttonInsertFile.UseVisualStyleBackColor = true;
+            buttonInsertFile.Click += buttonInsertFile_Click;
+            // 
+            // Server
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(842, 467);
+            Controls.Add(buttonInsertFile);
+            Controls.Add(labelShowStatus4);
+            Controls.Add(buttonAuth);
+            Controls.Add(label2);
+            Controls.Add(listBoxServerData);
+            Controls.Add(label1);
+            Controls.Add(listBoxAllData);
+            Controls.Add(labelShowStatus3);
+            Controls.Add(labelShowStatus2);
+            Controls.Add(labelShowStatus1);
+            Controls.Add(labelShowStatus);
+            Name = "Server";
+            Text = "Server";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelShowStatus;
+        private Label labelShowStatus1;
+        private Label labelShowStatus2;
+        private Label labelShowStatus3;
+        private ListBox listBoxAllData;
+        private Label label1;
+        private ListBox listBoxServerData;
+        private Label label2;
+        private Button buttonAuth;
+        private Label labelShowStatus4;
+        private Button buttonInsertFile;
+    }
+}
